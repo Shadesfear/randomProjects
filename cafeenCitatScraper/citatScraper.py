9befaa090999fad78643edb56ed63d74cb40a1bb
Christopher Carman <chris@thecarmans.com>
2017-10-13 01:04:11 +0200

    Completly reformated code, run time now under 1 min using multithreading

--- cafeenCitatScraper/citatScraper.py
+++ cafeenCitatScraper/citatScraper.py
@@ -6,65 +6,85 @@ from itertools import islice
 import re
 import string
 import time
+import os
+import _thread
+import threading
 
-#Initial load of webpage to see number of iterations
-fp = urllib.request.urlopen("http://www.cafeen.org/")
+#Number of threads, can be adjustet
+nthreads = 5
+website = "http://www.cafeen.org/"
+
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      downloadCitat(citater)
+      print ("Exiting " + self.name)
+
+
+def connectCafeen(site):
+	fp = urllib.request.urlopen(site)
+	return fp
+
+
+def downloadCitat(citater):
+
+	r0 = re.compile(' (.*?)\'')
+	r1  = re.compile('<i>(.*?)</i>')
+	r2 = re.compile('">(.*?)</td>') 
+
+	while None in citater:
+		try:
+			fp = connectCafeen(website)
+		except Exception as e:
+			continue
+		mystr = fp.read().decode(errors = 'ignore').split('\n')
+		
+		for index, line in enumerate(mystr):
+			if 'class="citat"' in line:
+				citat       = str(mystr[index+5:index+6])
+				citatHvem   = str(mystr[index+9:index+10])
+				citatNumber = str(mystr[index+2:index+3])
+
+		citat       = r1.search(citat).group(1)
+		citatHvem   = r2.search(citatHvem).group(1)
+		citatNumber = r0.search(citatNumber).group(1)
+		cIndex      = re.findall('\d+',citatNumber) 
+
+
+		if citater[int(cIndex[0])] == None:
+			citater[int(cIndex[0])] = citatNumber + "\n" + citat + "\n" + citatHvem + "\n \n"
+			print(str(citater.count(None)) + "\n")
+		else:
+			continue
+
+	file2 = open("CafeenCitater.txt","w")
+	for line in citater:
+		try:
+			file2.write(line)
+		except Exception as e:
+			pass
+	file2.close()
+
+
+#Initial connect to see how many files to download 
+fp = connectCafeen(website)
 mystr = fp.read().decode(errors = 'ignore').split('\n')
-
-
-#Regular expressions to search for
-r  = re.compile('<i>(.*?)</i>')
-r0 = re.compile(' (.*?)\'')
-r2 = re.compile('">(.*?)</td>') 
-
-#Find the number of citats to download in the source code
 for index, line in enumerate(mystr):
 	if 'class="citat"' in line:
 		citatNumber = str(mystr[index+2:index+3])
-
-#Strip
-#citatNumber = r0.search(citatNumber).group(1)
 cIndex      = re.findall('\d+',citatNumber) 
-
 citater     = [None]*int(cIndex[1])
 
-#for i in range(index):
-while True:
 
 
-	try:
-		fp = urllib.request.urlopen("http://www.cafeen.org/")
-	except Exception as e:
-		continue
-	
-	mystr = fp.read().decode(errors = 'ignore').split('\n')
-	
-	for index, line in enumerate(mystr):
-		if 'class="citat"' in line:
-			citat       = str(mystr[index+5:index+6])
-			citatHvem   = str(mystr[index+9:index+10])
-			citatNumber = str(mystr[index+2:index+3])
 
-
-	citat       = r.search(citat).group(1)
-	citatHvem   = r2.search(citatHvem).group(1)
-	citatNumber = r0.search(citatNumber).group(1)
-	cIndex      = re.findall('\d+',citatNumber) 
-
-
-	if citater[int(cIndex[0])] == None:
-		citater[int(cIndex[0])] = citatNumber + "\n" + citat + "\n" + citatHvem + "\n \n"
-		print("Added citat number " + cIndex[0] + "\n number of items left " + str(citater.count(None)) + "\n")
-	else:
-		continue
-
-	if None not in citater:
-		break
-
-
-file2 = open("CafeenCitater.txt","w")
-
-for line in citater:
-	file2.write(line)
-
-file2.close()
+#Gemerate n number of threads.
+for index, line in enumerate(range(1+nthreads)):
+	thread = "thread" + str(index)
+	thread = myThread(index+1,"thread"+str(index),index)
+	thread.start()
