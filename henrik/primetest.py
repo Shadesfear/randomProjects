103df119a23eaaddf79ad3e9256df26b546a50b7
Christopher Carman <chris@thecarmans.com>
2018-08-15 11:44:59 +0200

    something

--- henrik/primetest.py
+++ henrik/primetest.py
@@ -1,10 +1,13 @@
-import math
-print("")
 number = 300
+
 for num in range(2,number):
+    prime = False
+
     for i in range(2,num):
-        if (num % i ==0):
-            break
-    else:
+       if (num % i == 0):
+           prime = False
+           break
+       else:
+           prime = True
+    if (prime == True):
         print(num)
-imp
