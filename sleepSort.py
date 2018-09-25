04bb08bb2df9d1e61a06a2e44aaa9a75a465c158
Christopher Carman <chris@thecarmans.com>
2018-09-25 22:03:38 +0200

    something

--- sleepSort.py
+++ sleepSort.py
@@ -2,6 +2,7 @@ import time
 import threading
 
 items = [2,4,5,2,10,1]
+threads = []
 
 class myThread (threading.Thread):
 
@@ -20,4 +21,7 @@ def sleepSort(x):
 
 for index, line in enumerate(items):
     thread = myThread(line)
+    threads.append(thread)
+
+for thread in threads:
     thread.start()
